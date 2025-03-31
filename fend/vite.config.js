993221d5b314cc0eed906ac9626cf810fdac7294
Ante JobPC <perisic.ante@gmail.com>
2025-03-31 11:57:47 +0000

    work

--- fend/vite.config.js
+++ fend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
   allowedHosts: ['all','https://ina.plovput.hr'],
-  port: 3000,},
+  port: 3001,},
   
   plugins: [react()],
 })
